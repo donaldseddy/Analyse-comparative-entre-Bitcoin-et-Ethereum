5871b5e1489f1712712e3a0de7856d2f1c7207ab
Your Name <you@example.com>
2025-03-24 06:28:24 +0100

    amelioration code avec pappel fonction dans notebook

--- fonctions/collect_data.py
+++ fonctions/collect_data.py
@@ -1,11 +1,18 @@
 import yfinance as yf
 
-'''fonction de recuperation des donnes de monnaie avec l'API de yahoofinance
-    parametre: ticker: la devise desire en type (chaine de caractere) , 
-         period_start: date des debut devaluation en type (Date)  '''
-def get_crypto_data(ticker,period_start):
-    crypto = yf.Ticker(ticker)
-    hist = crypto.history(start=period_start)  
-    return hist
+def get_crypto_data(ticker, period_start):
+    """
+    Retrieves cryptocurrency data using the Yahoo Finance API.
 
+    Parameters:
+    ticker (str): The cryptocurrency ticker symbol (e.g., 'BTC-USD', 'ETH-USD').
+    period_start (str): The start date for data collection in 'YYYY-MM-DD' format.
+
+    Returns:
+    pandas.DataFrame: A DataFrame containing historical cryptocurrency data.
+    The DataFrame includes columns for 'Open', 'High', 'Low', 'Close', 'Volume', and 'Dividends'.
+    """
+    crypto = yf.Ticker(ticker)
+    hist = crypto.history(start=period_start)
+    return hist
 
--- fonctions/fonction_calcul.py
+++ fonctions/fonction_calcul.py
@@ -1,11 +1,24 @@
-'''fonction permetant de calculer le score et retourne la colonne du datafame calcule
-    parameter: df:dataframe
-                value: selection de la colonne a calculer
-                period :period d'analyse
-'''
-def calcul_z_scrore(df,value:str,period:int):
-    rolling_mean=df[value].rolling(window=period).mean()
-    rolling_std=df[value].rolling(window=period).std()
-    z_scrore=(df[value] - rolling_mean)/rolling_std
-    df['Z_SCRORE']=z_scrore
+import pandas as pd
+
+
+
+def calcul_z_scrore(df, value: str, period: int) -> pd.DataFrame:
+    """
+    This function calculates the Z-score for a given column in a DataFrame over a specified period.
+    The Z-score is a statistical measure that indicates how many standard deviations a data point is from the mean.
+    It helps in identifying outliers and understanding the data distribution.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame containing the data.
+    value (str): The name of the column in the DataFrame for which the Z-score needs to be calculated.
+    period (int): The period over which to calculate the rolling mean and standard deviation.
+
+    Returns:
+    pd.DataFrame: The input DataFrame with an additional column 'Z_SCRORE' containing the calculated Z-scores.
+    """
+    rolling_mean = df[value].rolling(window=period).mean()
+    rolling_std = df[value].rolling(window=period).std()
+    z_score = (df[value] - rolling_mean) / rolling_std
+    df['Z_SCRORE'] = z_score
     return df
+
